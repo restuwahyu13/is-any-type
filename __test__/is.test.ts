17ab568f332da7352ccdac14d5a73541b6b0d138
anti-malware <restuwahyu13@gmail.com>
2021-02-14 02:46:42 +0700

    update new test and new feature

--- __test__/is.test.ts
+++ __test__/is.test.ts
@@ -13,10 +13,34 @@ describe('is.test.ts', () => {
 		expect(type).toBe('string')
 	})
 
+	test('string match value from string', () => {
+		const type = is('hello wordl', 'hello wordl')
+		expect(type).toBeDefined()
+		expect(type).toBeTruthy()
+	})
+
+	test('string not match value from string', () => {
+		const type = is('hello wordl', 2021)
+		expect(type).toBeDefined()
+		expect(type).toBeFalsy()
+	})
+
 	test('is instance value is from number', () => {
 		const type = is(2021)
 		expect(type).toBeDefined()
 		expect(type).toBe('number')
+	})
+
+	test('number match value from number', () => {
+		const type = is(2021, 2021)
+		expect(type).toBeDefined()
+		expect(type).toBeTruthy()
+	})
+
+	test('number not match value from number', () => {
+		const type = is(2021, 'hello wordl')
+		expect(type).toBeDefined()
+		expect(type).toBeFalsy()
 	})
 
 	test('is instance value is from object', () => {
@@ -25,10 +49,34 @@ describe('is.test.ts', () => {
 		expect(type).toBe('object')
 	})
 
+	test('object match value from object', () => {
+		const type = is({ name: 'john doe' }, { name: 'jane doe' })
+		expect(type).toBeDefined()
+		expect(type).toBeTruthy()
+	})
+
+	test('object not match value from object', () => {
+		const type = is({ name: 'john doe' }, 2021)
+		expect(type).toBeDefined()
+		expect(type).toBeFalsy()
+	})
+
 	test('is instance value is from array', () => {
 		const type = is([1, 2, 3, 4, 5])
 		expect(type).toBeDefined()
 		expect(type).toBe('array')
+	})
+
+	test('array match value from array', () => {
+		const type = is([1, 2, 3, 4, 5], [6, 7, 8, 9, 10])
+		expect(type).toBeDefined()
+		expect(type).toBeTruthy()
+	})
+
+	test('array not match value from array', () => {
+		const type = is([1, 2, 3, 4, 5], 2021)
+		expect(type).toBeDefined()
+		expect(type).toBeFalsy()
 	})
 
 	test('is instance value is from function', () => {
@@ -37,10 +85,34 @@ describe('is.test.ts', () => {
 		expect(type).toBe('function')
 	})
 
+	test('function match value from function', () => {
+		const type = is(() => 'hello wordl', Function)
+		expect(type).toBeDefined()
+		expect(type).toBeTruthy()
+	})
+
+	test('function not match value from function', () => {
+		const type = is(() => 'hello wordl', 2021)
+		expect(type).toBeDefined()
+		expect(type).toBeFalsy()
+	})
+
 	test('is instance value is from promise', () => {
 		const type = is(Promise.resolve('hello wordl'))
 		expect(type).toBeDefined()
 		expect(type).toBe('promise')
+	})
+
+	test('promise match value from promise', () => {
+		const type = is(Promise.resolve('hello wordl'), Promise)
+		expect(type).toBeDefined()
+		expect(type).toBeTruthy()
+	})
+
+	test('promise not match value from promise', () => {
+		const type = is(Promise.resolve('hello wordl'), 2021)
+		expect(type).toBeDefined()
+		expect(type).toBeFalsy()
 	})
 
 	test('is instance value is from buffer', () => {
@@ -49,10 +121,34 @@ describe('is.test.ts', () => {
 		expect(type).toBe('buffer')
 	})
 
+	test('buffer match value from buffer', () => {
+		const type = is(Buffer.from('hello wordl'), Buffer.from('hello john'))
+		expect(type).toBeDefined()
+		expect(type).toBeTruthy()
+	})
+
+	test('buffer not match value from buffer', () => {
+		const type = is(Buffer.from('hello wordl'), 2021)
+		expect(type).toBeDefined()
+		expect(type).toBeFalsy()
+	})
+
 	test('is instance value is from null', () => {
 		const type = is(null)
 		expect(type).toBeDefined()
 		expect(type).toBe('null')
+	})
+
+	test('null match value from null', () => {
+		const type = is(null, null)
+		expect(type).toBeDefined()
+		expect(type).toBeTruthy()
+	})
+
+	test('null not match value from null', () => {
+		const type = is(null, 2021)
+		expect(type).toBeDefined()
+		expect(type).toBeFalsy()
 	})
 
 	test('is instance value is from undefined', () => {
@@ -62,9 +158,33 @@ describe('is.test.ts', () => {
 		expect(type).toBe('undefined')
 	})
 
+	test('undefined match value from undefined', () => {
+		const type = is(undefined, undefined)
+		expect(type).toBeDefined()
+		expect(type).toBeTruthy()
+	})
+
+	test('undefined not match value from undefined', () => {
+		const type = is(undefined, 2021)
+		expect(type).toBeDefined()
+		expect(type).toBeFalsy()
+	})
+
 	test('is instance value is from boolean', () => {
 		const type = is(true)
 		expect(type).toBeDefined()
 		expect(type).toBe('boolean')
+	})
+
+	test('boolean match value from boolean', () => {
+		const type = is(true, false)
+		expect(type).toBeDefined()
+		expect(type).toBeTruthy()
+	})
+
+	test('boolean not match value from boolean', () => {
+		const type = is(true, 2021)
+		expect(type).toBeDefined()
+		expect(type).toBeFalsy()
 	})
 })
--- src/lib/is.ts
+++ src/lib/is.ts
@@ -1,4 +1,4 @@
-export default function isType(data: any): string {
+export default function isType(data: any, compare?: any): string | boolean {
 	const isArray: string = Array.isArray(data) && 'array'
 	const isBuffer: string = Buffer.isBuffer(data) && 'buffer'
 	const isPromise: string = data instanceof Promise && 'promise'
@@ -7,10 +7,34 @@ export default function isType(data: any): string {
 	const isNull: string = data === null && 'null'
 	const isUndefined: string = data === undefined && 'undefined'
 
+	const isArrayCompare: boolean = Array.isArray(data) && Array.isArray(compare)
+	const isBufferCompare: boolean = Buffer.isBuffer(data) && Buffer.isBuffer(compare)
+	const isPromiseCompare: boolean = data instanceof Promise && compare instanceof Promise
+	const isFunctionCompare: boolean = data instanceof Function && compare instanceof Function
+	const isObjectCompare: boolean = Object.isExtensible(data) && Object.isExtensible(compare)
+	const isNullCompare: boolean = data === null && compare === null
+	const isUndefinedCompare: boolean = data === undefined && compare === undefined
+	const isStringCompare: boolean = typeof data === 'string' && typeof compare === 'string'
+	const isNumberCompare: boolean = typeof data === 'number' && typeof compare === 'number'
+	const isBooleanCompare: boolean = data instanceof Boolean && compare instanceof Boolean
+
 	const isGroup: string =
 		isArray || isBuffer || isPromise || isFunction || isObject || isNull || isUndefined
 
+	const isGroupCompare =
+		isArrayCompare ||
+		isBufferCompare ||
+		isPromiseCompare ||
+		isFunctionCompare ||
+		isObjectCompare ||
+		isNullCompare ||
+		isUndefinedCompare ||
+		isStringCompare ||
+		isNumberCompare ||
+		isBooleanCompare
+
 	const isCheck: string = isGroup || typeof data
+	const isCheckCompare: boolean = isGroupCompare || false
 
 	const isTypeData: string[] = [
 		'array',
@@ -27,6 +51,7 @@ export default function isType(data: any): string {
 
 	const isMatch: number = isTypeData.indexOf(isCheck)
 	const isResult: string = isTypeData[isMatch]
+	const isCompare: string | boolean = !compare ? isResult : isCheckCompare
 
-	return isResult
+	return isCompare
 }
